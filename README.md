0d1791ad8aa6e5ba612a067308f5fe914d7145a4
Zou Yi <zouyi.nju@gmail.com>
2022-01-24 12:47:50 +0800

    modify main script

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # hifisr
+
 HiFi-SR is a Python-based pipeline for the detection of plant mitochondrial structural rearrangements based on the mapping of HiFi reads, or PacBio Circular Consensus Sequencing (ccs) data, to a reference genome (e.g., hypothetical master cycle DNA).
+
+**Deps**
+
+The pipeline has been tested in WSL2 distribution Ubuntu-20.04. It should work in other Linux operating system, such as CentOS.
+
+* Download the hifisr repository
+
+```
+git clone https://github.com/zouyinstein/hifisr
+```
+
+* Create and activate a conda environment.
+
+```bash
+conda create -n hifisr python=3.9
+conda activate hifisr
+```
+
+* Use Anaconda3 to install required packages.
+
+```bash
+conda install pigz -c conda-forge
+conda install samtools bamtools blast seqkit -c bioconda
+ln -sf /home/zouyi/.conda/envs/hifisr/lib/libcrypto.so.1.1 /home/zouyi/.conda/envs/hifisr/lib/libcrypto.so.1.0.0  # create a soft link
+```
+
+* Install minimap2
+
+```bash
+cd hifisr/deps
+curl -L https://github.com/lh3/minimap2/releases/download/v2.24/minimap2-2.24_x64-linux.tar.bz2 | tar -jxvf -
+./minimap2-2.24_x64-linux/minimap2
+export PATH=/mnt/e/02.vol1/03.work/03.HiFi/hifisr/deps/minimap2-2.24_x64-linux:$PATH
+```
+
+* Install required Python packages
+
+```bash
+pip install biopython
+```
+
+**Example**
+
+Analyze of an example wild-type *Arabidopsis thaliana* Col-0 dataset (ERR6210723, 14.6 Gb, Naish et al., 2021, Science): 
+
+```bash
+cd hifisr/pre
+mkdir CEN  # CEN is the sample name
+cd CEN
+pigz -d -k -p 16 ERR6210723.fastq.gz
+ln -sf ERR6210723.fastq CEN.fastq
+# manually change the FASTA headers of mitochondrial and plastid genome refercences into mito and plastid for easily manipulation
+cat Athaliana_447_TAIR10.id_Chr{1,2,3,4,5}.fa refs_cp28673mod.id_mito.fas refs_cp28673mod.id_plastid.fas > CEN_ref.fa
+ln -sf refs_cp28673mod.id_mito.fas CEN_mito.fa
+ln -sf refs_cp28673mod.id_plastid.fas CEN_plastid.fa
+bash ../../run_hifisr.sh CEN 16 fastq &
+
+```
--- run_hifisr.sh
+++ run_hifisr.sh
@@ -1,49 +1,12 @@
-sample=$1
-thread=$2
-input_type=$3
-type_number=5
+sample=$1      # sample name, for example CEN
+thread=$2      # number of threads to run the pipeline
+input_type=$3  # fastq or fasta
+type_number=5  # analyze reads up to four rearrangements in a single read
 
-export PATH=$PATH:/usr/software/minimap2-2.20_x64-linux/
-export PATH=$PATH:/usr/software/anaconda3/bin/
-export PATH=/usr/software/Python-3.9.9/bin/:$PATH
-
-minimap2 -t ${thread} -ax map-hifi ref.fa ${sample}.${input_type} > ${sample}.sam
-samtools view -Sb -@ ${thread} ${sample}.sam > ${sample}.bam
-samtools sort ${sample}.bam -@ ${thread} > ${sample}.sorted.bam
-samtools index -@ ${thread} ${sample}.sorted.bam
-samtools flagstat ${sample}.sorted.bam -@ ${thread} > ${sample}.stat
-bamtools split -in ${sample}.sorted.bam -reference
-find ./ -name "*.REF_*.bam" > split_bam.list
-cat split_bam.list | cut -c3- | while read i; do samtools fastq $i -@ ${thread} -0 ${i/.bam/}.fastq; done
-find ./ -name "*.REF_*.fastq" > split_fastq.list_all
-cat split_fastq.list_all | cut -c3- | while read i; do seqkit fq2fa $i -j ${thread} -o ${i/.fastq/}.fasta; seqkit stat -T -a -j ${thread} $i > $i.stat; done
-makeblastdb -dbtype nucl -parse_seqids -in ${sample}_mito.fa
-makeblastdb -dbtype nucl -parse_seqids -in ${sample}_plastid.fa
-blastn -query ${sample}.sorted.REF_mito.fasta -db ${sample}_mito.fa -num_threads ${thread} -outfmt 5 -out blastn_${sample}_mito.xml
-blastn -query ${sample}.sorted.REF_plastid.fasta -db ${sample}_plastid.fa -num_threads ${thread} -outfmt 5 -out blastn_${sample}_plastid.xml
-python ../../scripts/analysis_blastn.py blastn_${sample}_mito.xml > blastn_result_${sample}_mito.txt
-python ../../scripts/analysis_blastn.py blastn_${sample}_plastid.xml > blastn_result_${sample}_plastid.txt
-python ../../scripts/get_type.py blastn_result_${sample}_mito.txt > blastn_type_count_result_${sample}_mito.txt
-seq 1 ${type_number} | while read i; do grep "type_${i}" blastn_type_count_result_${sample}_mito.txt > blastn_type_${i}_result_${sample}_mito.txt; python ../../scripts/get_type_${i}.py blastn_type_${i}_result_${sample}_mito.txt > type_${i}_all_${sample}_mito.txt; done
-python scripts/get_type.py blastn_result_${sample}_plastid.txt > blastn_type_count_result_${sample}_plastid.txt
-seq 1 ${type_number} | while read i; do grep "type_${i}" blastn_type_count_result_${sample}_plastid.txt > blastn_type_${i}_result_${sample}_plastid.txt; python ../../scripts/get_type_${i}.py blastn_type_${i}_result_${sample}_plastid.txt > type_${i}_all_${sample}_plastid.txt; done
-pigz -p ${thread} ${sample}.${input_type}
-rm *sam *bam *bai split*
-mkdir -p {mito,plastid,reads,blastn}
-mv type_*_all_${sample}_mito.txt mito
-mv type_*_all_${sample}_plastid.txt plastid
-mv ${sample}.${input_type}.gz reads
-mv ${sample}* reads
-mv blastn* blastn
-cd mito
-cut -f3 type_*_all_${sample}_mito.txt | sort -u > mito_reads_type.list
-cat mito_reads_type.list | while read i; do mkdir "$i"; grep "$i" type_*_all_${sample}_mito.txt > ${i}/${i}_${sample}_mito.txt; wc -l ${i}/${i}_${sample}_mito.txt; done
-cd ../plastid
-cut -f3 type_*_all_${sample}_plastid.txt | sort -u > plastid_reads_type.list
-cat plastid_reads_type.list | while read i; do mkdir "$i"; grep "$i" type_*_all_${sample}_plastid.txt > ${i}/${i}_${sample}_plastid.txt; wc -l ${i}/${i}_${sample}_plastid.txt; done
-cd ..
-cd mito
-cat mito_reads_type.list | grep "type_2" | while read sub_type; do cd ${sub_type}; python ../../../../scripts/analysis_type_2.py ${sub_type}_${sample}_mito.txt ${sample}; cd ..; done
-cd ../plastid
-cat plastid_reads_type.list | grep "type_2" | while read sub_type; do cd ${sub_type}; python ../../../../scripts/analysis_type_2.py ${sub_type}_${sample}_plastid.txt ${sample}; cd ..; done
-cd ..
+# bash ../../scripts/run_stage_all.sh $sample $thread $input_type
+# bash ../../scripts/run_stage_1.sh $sample $thread $input_type
+# bash ../../scripts/run_stage_2.sh $sample $thread $input_type
+# bash ../../scripts/run_stage_3.sh $sample $thread $input_type
+# bash ../../scripts/run_stage_4.sh $sample $thread $input_type
+# bash ../../scripts/run_stage_5.sh $sample $thread $input_type
+bash ../../scripts/run_stage_test.sh $sample $thread $input_type
