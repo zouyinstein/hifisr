822a6e4524f21bdc315d28612cfaeacf1cef1fc9
zouyinstein <zouyi.nju@gmail.com>
2022-11-09 16:18:48 +0800

    update

--- scripts/run_stage_all.sh
+++ scripts/run_stage_all.sh
@@ -27,12 +27,14 @@ python ../../scripts/write_blastn_commands.py ${sample} mito
 python ../../scripts/write_blastn_commands.py ${sample} plastid
 find mito_commands/ -name "*sh" > mito_commands.txt
 find plastid_commands/ -name "*sh" > plastid_commands.txt
-cat mito_commands.txt | parallel '{}' -j ${thread}
-cat plastid_commands.txt | parallel '{}' -j ${thread}
+cat mito_commands.txt | while read i; do echo "sh " $i >> mito_commands.list; done
+cat plastid_commands.txt | while read i; do echo "sh " $i >> plastid_commands.list; done
+cat mito_commands.list | parallel '{}' -j ${thread}
+cat plastid_commands.list | parallel '{}' -j ${thread}
 find tmp/ -name "blastn_*mito*.txt" > blastn_${sample}_mito.list
-cat blastn_${sample}_mito.txt | while read i; do cat $i >> blastn_result_${sample}_mito.txt
+cat blastn_${sample}_mito.txt | while read i; do cat $i >> blastn_result_${sample}_mito.txt; done
 find tmp/ -name "blastn_*plastid*.txt" > blastn_${sample}_plastid.list
-cat blastn_${sample}_plastid.txt | while read i; do cat $i >> blastn_result_${sample}_plastid.txt
+cat blastn_${sample}_plastid.txt | while read i; do cat $i >> blastn_result_${sample}_plastid.txt; done
 
 # process combined blastn results
 python ../../scripts/get_type.py blastn_result_${sample}_mito.txt > blastn_type_count_result_${sample}_mito.txt
--- scripts/write_blastn_commands.py
+++ scripts/write_blastn_commands.py
@@ -8,4 +8,4 @@ for id in ids:
     ID = "__".join(id.rstrip("\n").split("/"))
     with open(sys.argv[2] + "_commands/runme_" + ID + ".sh", "at") as fout:
         print("blastn -query " + sample + ".sorted.REF_" + sys.argv[2] + ".fasta.split/" + sample + ".sorted.REF_" + sys.argv[2] + ".id_" + ID + ".fasta -db " + sample + "_" + sys.argv[2] + ".fa -outfmt 5 -out tmp/blastn_" + sample + "_" + sys.argv[2] + ID + "_" + ".xml", file=fout)
-        print("python ../../scripts/analysis_blastn.py tmp/blastn_" + sample + "_" + sys.argv[2] + ID + "_" + ".xml " + sample + "_" + sys.argv[2] + ".fa > tmp/blastn_" + sample + "_" + sys.argv[2] + ID + "_" + ".txt", file=fout)
+        print("python ../../scripts/analysis_blastn.py tmp/blastn_" + sample + "_" + sys.argv[2] + "_" + ID + "_" + ".xml " + sample + "_" + sys.argv[2] + ".fa > tmp/blastn_" + sample + "_" + sys.argv[2] + ID + ".txt", file=fout)
