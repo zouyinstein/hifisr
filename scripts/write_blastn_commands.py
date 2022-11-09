df81d441c41621f6d972e2ba6c4eecbd1079300e
zouyinstein <zouyi.nju@gmail.com>
2022-11-09 16:26:16 +0800

    Update write_blastn_commands.py

--- scripts/write_blastn_commands.py
+++ scripts/write_blastn_commands.py
@@ -7,5 +7,5 @@ with open("all_" + sys.argv[2] + ".ids", "rt") as fin:
 for id in ids:
     ID = "__".join(id.rstrip("\n").split("/"))
     with open(sys.argv[2] + "_commands/runme_" + ID + ".sh", "at") as fout:
-        print("blastn -query " + sample + ".sorted.REF_" + sys.argv[2] + ".fasta.split/" + sample + ".sorted.REF_" + sys.argv[2] + ".id_" + ID + ".fasta -db " + sample + "_" + sys.argv[2] + ".fa -outfmt 5 -out tmp/blastn_" + sample + "_" + sys.argv[2] + ID + "_" + ".xml", file=fout)
-        print("python ../../scripts/analysis_blastn.py tmp/blastn_" + sample + "_" + sys.argv[2] + "_" + ID + "_" + ".xml " + sample + "_" + sys.argv[2] + ".fa > tmp/blastn_" + sample + "_" + sys.argv[2] + ID + ".txt", file=fout)
+        print("blastn -query " + sample + ".sorted.REF_" + sys.argv[2] + ".fasta.split/" + sample + ".sorted.REF_" + sys.argv[2] + ".id_" + ID + ".fasta -db " + sample + "_" + sys.argv[2] + ".fa -outfmt 5 -out tmp/blastn_" + sample + "_" + sys.argv[2] + ID + ".xml", file=fout)
+        print("python ../../scripts/analysis_blastn.py tmp/blastn_" + sample + "_" + sys.argv[2] + "_" + ID + ".xml " + sample + "_" + sys.argv[2] + ".fa > tmp/blastn_" + sample + "_" + sys.argv[2] + ID + ".txt", file=fout)
